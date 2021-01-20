d78d5b98e8ff4d6985139862eb2ce99e75613967
bawilderman1 <30907764+bawilderman1@users.noreply.github.com>
2021-01-20 00:30:33 -0600

    Got IOptions stubbed out and working

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration.Json;
+using StockDataEtl.models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +17,31 @@ namespace StockDataEtl
     {
         static void Main(string[] args)
         {
+            var envVars = Environment.GetEnvironmentVariables();
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            // create service collection
+            var services = new ServiceCollection();
+
+            // build config
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false)
+                .AddJsonFile($"appsettings.{environmentName}.json", true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            // setup config
+            services.AddOptions();
+            services.Configure<AppSettings>(configuration.GetSection("App"));
+
+            // create service provider
+            var serviceProvider = services.BuildServiceProvider();
+
+            var appSettings = serviceProvider.GetService<IOptions<AppSettings>>();
+            var appSettingsValue = appSettings.Value;
+
+            Console.WriteLine();
         }
     }
 }
---
+++ models/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockDataEtl.models
+{
+    public class AppSettings
+    {
+        public string TestProp { get; set; }
+    }
+}
